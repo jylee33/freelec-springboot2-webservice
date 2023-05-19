24e8b64c5d723bfdf4454707a27574db5b2b44a9
yubbi33 <yubbi33@gmail.com>
2023-05-19 17:40:14 +0900

    게시판 등록/수정/삭제 기능 추가 및 테스트 코드 완료

--- src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-testimport static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
