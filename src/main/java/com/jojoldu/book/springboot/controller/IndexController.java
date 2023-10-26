eaf4014d8dcedf6059bb96aab6f17fc1ceb5928b
yubbi33 <yubbi33@gmail.com>
2023-10-26 16:55:27 +0900

    구글 로그인 기능

--- src/main/java/com/jojoldu/book/springboot/controller/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.jojoldu.book.springboot.controller;
 
+import com.jojoldu.book.springboot.config.auth.dto.SessionUser;
 import com.jojoldu.book.springboot.controller.dto.PostsResponseDto;
 import com.jojoldu.book.springboot.services.posts.PostsService;
 import lombok.RequiredArgsConstructor;
@@ -8,16 +9,26 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         System.out.println("Called index ------------------");
+
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
